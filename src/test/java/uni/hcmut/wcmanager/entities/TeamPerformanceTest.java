5abe52becc6cf10591bb659201629a0152b4f75e
ThaoTrinh <trinhthithuthaopy@gmail.com>
2019-11-12 22:15:25 +0700

    add_test

--- src/test/java/uni/hcmut/wcmanager/entities/TeamPerformanceTest.java
+++ src/test/java/uni/hcmut/wcmanager/entities/TeamPerformanceTest.java
@@ -3,7 +3,11 @@ package uni.hcmut.wcmanager.entities;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import uni.hcmut.wcmanager.constants.GameRule;
 import uni.hcmut.wcmanager.enums.GroupName;
+import uni.hcmut.wcmanager.events.Event;
+import uni.hcmut.wcmanager.events.RedCardEvent;
+import uni.hcmut.wcmanager.events.YellowCardEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,51 +15,56 @@ import java.util.List;
 public class TeamPerformanceTest {
 
     Group group = new Group(GroupName.A);
-    Team team1 = new Team();
-    Team team2 = new Team();
-    Team team3 = new Team();
-    List<Team> listTeam = new ArrayList<>();
-
-
+    Team TeamHome = new Team();
+    Team TeamAway = new Team();
+    List<Player> playerHome = new ArrayList<Player>();
+    List<Player> playerAway = new ArrayList<Player>();
+    Match match;
     @Before
     public void init() {
-        Player player1 = new Player();
-        Player player2 = new Player();
+        for(int index = 0; index < GameRule.TEAM_PLAYER_COUNT; index ++){
+            Player player = new Player();
+            String fullName = "TeamHome" + Integer.toString(index);
+            player.setFullname(fullName);
+            player.setGoalCount(index%2);
+            playerHome.add(player);
 
-        player1.setFullname("Nguyen Trong Nghia");
-        player1.setGoalCount(3);
+            Player player2 = new Player();
+            String fullNameAway = "TeamAway" + Integer.toString(index);
+            player2.setFullname(fullNameAway);
+            player2.setGoalCount(index%3);
+            playerAway.add(player2);
+        }
 
-        player2.setFullname("Tran Dang Khoi");
-        player2.setGoalCount(4);
+        TeamHome.setPlayers(playerHome);
+        TeamAway.setPlayers(playerAway);
+        group.addTeam(TeamHome);
+        group.addTeam(TeamAway);
 
-        List<Player> playerList1 = new ArrayList<Player>();
-        playerList1.add(player1);
-
-        List<Player> playerList2 = new ArrayList<Player>();
-        playerList2.add(player2);
-
-        team1.setPlayers(playerList1);
-        team2.setPlayers(playerList2);
-        team3.setPlayers(playerList2);
-
-        listTeam.add(team1);
-        listTeam.add(team2);
-
-        group.addTeam(team1);
-        group.addTeam(team2);
-
+        match = new DrawableMatch(TeamHome, TeamAway);
     }
 
     @Test
     public void Test_getTeam(){
-        TeamPerformance teamPerformances = new TeamPerformance(team1, group);
-        Assert.assertEquals(team1, teamPerformances.getTeam());
+        TeamPerformance teamPerformances = new TeamPerformance(TeamHome, group);
+        Assert.assertEquals(TeamHome, teamPerformances.getTeam());
     }
 
     @Test
     public void Test_getWrongTeam(){
-        TeamPerformance teamPerformances = new TeamPerformance(team1, group);
-        Assert.assertNotEquals(team2, teamPerformances.getTeam());
+        TeamPerformance teamPerformances = new TeamPerformance(TeamHome, group);
+        Assert.assertNotEquals(TeamAway, teamPerformances.getTeam());
+    }
+
+    @Test
+    public void Test_Update_TeamHome(){
+        TeamPerformance teamPerformances = new TeamPerformance(TeamHome, group);
+        TeamInMatch team1 = new TeamInMatch(TeamHome);
+        PlayerInMatch playerInMatch = new PlayerInMatch(playerHome.get(0), team1);
+        Event RedCard = new YellowCardEvent(match, playerInMatch, 10);
+        match.handleEvent(RedCard);
+        teamPerformances.update(match);
+        Assert.assertEquals(1, teamPerformances.getYellowCard());
     }
 
 
---
+++ src/test/java/uni/hcmut/wcmanager/enums/RoundNameTest.java
@@ -0,0 +1,10 @@
+package uni.hcmut.wcmanager.enums;
+
+import org.junit.Test;
+
+public class RoundNameTest {
+    @Test
+    public void test_getID(){
+
+    }
+}
